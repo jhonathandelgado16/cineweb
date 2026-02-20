7a406f4a9360d5becaaa840cd8d5a71a2ef819fb
jhonathandelgado16 <jhonathan.delgado.santos@gmail.com>
2026-02-20 08:15:44 -0300

    modified Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Button = (props) => {
+  function handleClick() {
+    alert(props.textButton);
+  }
   return (
-    <button className="bg-[#d00000] hover:bg-[#a00000] p-2 px-4 rounded-lg text-white text-sm">
+    <button
+      className="bg-[#d00000] hover:bg-[#a00000] p-2 px-4 rounded-lg text-white text-sm"
+      onClick={handleClick}
+    >
       {props.textButton}
     </button>
   );
